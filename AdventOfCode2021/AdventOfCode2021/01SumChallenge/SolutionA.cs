a8bac39542e7490292eca67e39fecdb12f338f83
tait <tai.tang@thetrainline.com>
2021-12-02 19:50:37 +0000

    01SumChallenge

---
+++ AdventOfCode2021/AdventOfCode2021/01SumChallenge/SolutionA.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode2021._01SumChallenge
+{
+    public class SolutionA
+    {
+        public static void Run()
+        {
+            var data = new SolutionAData().GetInput().ToArray();
+
+            var pointer = 0;
+            int lastSum = -1;
+            var counter = 0;
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data.Length - pointer >= 3)
+                {
+                    var sum = data[pointer] + data[pointer + 1] + data[pointer + 2];
+
+                    if (lastSum != -1)
+                    {
+                        if (sum > lastSum)
+                        {
+                            counter++;
+                        }
+                    }
+
+                    pointer++;
+                    lastSum = sum;
+                }
+            }
+
+            Console.WriteLine(counter);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AdventOfCode2021/AdventOfCode2021/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AdventOfCode2021._01SumChallenge;
+
+namespace AdventOfCode2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SolutionA.Run();
+
+            Console.ReadKey();
+        }
+    }
+}
